e688a93980aaf280fd537ca8ebba74a95c2f3551
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-29 09:45:05 +0700

    repair function insert number

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 let arr = [2, 3, 4, 6, 7, 8];
-let arr1 = [];
-let arr2 = [];
-let arrnew = [];
 let number = Number(prompt("Import number"));
 
 function insertNumber() {
@@ -9,23 +6,14 @@ function insertNumber() {
     for (let i = 0; i < arr.length; i++) {
         if (number < arr[0]) {
             arr.unshift(number);
-            return console.log(arr);
+            return arr;
         } else if (number >= arr[i] && number < arr[i + 1]) {
-            for (let j = 0; j <= i; j++) {
-                arr1.push(arr[j]);
-            }
-            arr1.push(number);
-            for (let k = i + 1; k < arr.length; k++) {
-                arr2.push(arr[k]);
-            }
+            arr.splice(i + 1, 0, number);
         } else if (number > arr[arr.length - 1]) {
-            arr.push(number)
-            return console.log(arr);
+            arr.push(number);
+            return arr;
         }
     }
-    arrnew = arr1.concat(arr2);
-    console.log(arrnew);
-
 }
 
-insertNumber();
+console.log(insertNumber());
